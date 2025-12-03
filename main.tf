cec162f02ecf811eaa6e47f740123989da3790e8
apipankowska-gif <api.pankowska@gmail.com>
2025-12-03 09:45:33 +0000

    x

--- main.tf
+++ main.tf
@@ -1,8 +1,35 @@
-variable "server-name" {
-  type        = string
-  description = "name of a serwer"
+# Write a calculator
+
+variable "a" {
+  type = number
+    validation {
+    condition     = var.a > 0
+    error_message = "Zmienna powinna być liczbą"
+  }
 }
 
-output "out" {
-  value = var.server-name
+variable "b" {
+  type = number
+}
+
+# Allowed: +, -, *, /
+variable "operand" {
+  type = string
+  validation {
+    condition     = contains (["+", "-", "*", "/"], var.operand)
+    error_message = "Niewłaściwy operator"
+  }
+  
+}
+
+locals {
+  dodaj = var.a + var.b  
+  odejmij = var.a - var.b  
+  pomnoz = var.a * var.b  
+  podziel = var.a / var.b   
+  resultTmp  = var.operand == "+" ? local.dodaj : var.operand == "-" ? local.odejmij : var.operand == "*" ? local.pomnoz : var.operand == "/" ? local.podziel : 0
+ }
+
+output "result" {
+  value = local.resultTmp
 }
