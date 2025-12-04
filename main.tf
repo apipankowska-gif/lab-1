6c0799183b17441a9a256e3209f1bbbd2ef596ba
apipankowska-gif <api.pankowska@gmail.com>
2025-12-04 06:14:14 +0000

    xxx

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
 # Write a calculator
 
-variable "a" {
-  type = number
-    validation {
-    condition     = var.a > 0
-    error_message = "Zmienna powinna być liczbą"
-  }
-}
-
-variable "b" {
-  type = number
-}
-
 # Allowed: +, -, *, /
 variable "operand" {
   type = string
@@ -19,17 +7,29 @@ variable "operand" {
     condition     = contains (["+", "-", "*", "/"], var.operand)
     error_message = "Niewłaściwy operator"
   }
-  
+}
+
+variable "a" {
+  type = number
+}
+
+variable "b" {
+  type = number
 }
 
 locals {
+  nieZero = var.operand == "/" && var.b == 0
   dodaj = var.a + var.b  
   odejmij = var.a - var.b  
   pomnoz = var.a * var.b  
-  podziel = var.a / var.b   
+  podziel = local.nieZero == false ? var.a / var.b : 0
   resultTmp  = var.operand == "+" ? local.dodaj : var.operand == "-" ? local.odejmij : var.operand == "*" ? local.pomnoz : var.operand == "/" ? local.podziel : 0
- }
+}
 
 output "result" {
   value = local.resultTmp
+}
+
+output "resultVal" {
+  value = local.nieZero == true ? "Nie dziel przez ZERO !!!!" : "wynik gotowy !!!"
 }
